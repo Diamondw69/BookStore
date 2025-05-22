c44adbbbee36637c09d219fc75a654b6e928163e
almaz <almazsydykov768@gmail.com>
2025-05-22 12:38:16 +0500

    feat: authentification

--- src/main/resources/migration/V4__create_users_table.sql
+++ src/main/resources/migration/V4__create_users_table.sql
@@ -7,16 +7,3 @@ CREATE TABLE users (
                        created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
                        updated_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
-
-CREATE OR REPLACE FUNCTION set_updated_at_column()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = now();
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trg_set_updated_at
-    BEFORE UPDATE ON users
-    FOR EACH ROW
-    EXECUTE FUNCTION set_updated_at_column();
